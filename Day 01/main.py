057cbeba234e3edee933860333ad9d45ea9207c9
John Mullaney <j.a.mullaney1@gmail.com>
2023-03-22 22:05:14 -0700

    Notes for print and variables. Start project

---
+++ Day 01/main.py
@@ -0,0 +1,10 @@
+print("Hello World! \nHello World") # outputs whatever is inside paranthesis. 
+# \n helps us go to a new line
+# + concatenates the string
+
+
+# Day 1 Project Below
+
+print("Welcome to my combination game! Together we can find out what your band name would be. Please answer the following questions: ")
+name_of_town = input("What was the name of the city you grew up in? ")
+name_of_pet = input("What was the name of your first pet? ")
