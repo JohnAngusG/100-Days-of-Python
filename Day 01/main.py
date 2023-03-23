5552dfe0b67582e99e82a291eaafa0fb2b8391b6
John Mullaney <j.a.mullaney1@gmail.com>
2023-03-22 22:11:43 -0700

    Finish project and put inputs on new line.

--- Day 01/main.py
+++ Day 01/main.py
@@ -1,4 +1,4 @@
-print("Hello World! \nHello World") # outputs whatever is inside paranthesis. 
+# print("Hello World! \nHello World") # outputs whatever is inside paranthesis. 
 # \n helps us go to a new line
 # + concatenates the string
 
@@ -6,5 +6,7 @@ print("Hello World! \nHello World") # outputs whatever is inside paranthesis.
 # Day 1 Project Below
 
 print("Welcome to my combination game! Together we can find out what your band name would be. Please answer the following questions: ")
-name_of_town = input("What was the name of the city you grew up in? ")
-name_of_pet = input("What was the name of your first pet? ")
+name_of_town = input("What was the name of the city you grew up in? \n")
+name_of_pet = input("What was the name of your first pet? \n")
+
+print(f"Your band name is {name_of_pet} {name_of_town}")
