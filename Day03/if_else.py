4c35ebc4bb2154308000311c41161da01d563fb9
John Mullaney <j.a.mullaney1@gmail.com>
2023-04-08 13:05:30 -0700

    Set up if else and clean up variable type

---
+++ Day03/if_else.py
@@ -0,0 +1,9 @@
+#Day 03 focuses on control flow and logical operators. 
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height > 120:
+    print("You can ride the rollercoaster!")
+else:
+    print("You cannot ride the rollercoaster :(")
